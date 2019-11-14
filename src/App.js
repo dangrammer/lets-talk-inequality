69a22b6ff0883644829bb0a1d38d6e5317431338
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-11-14 16:27:30 -0500

    project deliverable needs validations and refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import GenderGapFacts from './components/GenderGapFacts';
 import AgeGapFacts from './components/AgeGapFacts';
 import RacialGapFacts from './components/RacialGapFacts';
+import {Route, Switch, NavLink, Redirect, withRouter} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -10,16 +11,22 @@ class App extends Component {
     <main className="App">
       <header>
         <h1 id="intro-heading">Now that we are part of the tech industry, we need to be aware of the problems it has, and start thinking of how to solve them</h1>
+          <NavLink to={'/'}>Gender Gap facts</NavLink><br/>
+          <NavLink to={'/agegapfacts'}>Age Gap facts</NavLink><br/>
+          <NavLink to={'/racialgapfacts'}>Racial Gap facts</NavLink><br/>
       </header>
       <section>
         <p>Now that we know the problem. Let's be part of the solution </p>
         <img id='inequality-logo' src='https://www.equalitytrust.org.uk/sites/default/files/FI_logo.jpg' alt='inequality-logo'/>
       </section>
-      <GenderGapFacts />
-      <AgeGapFacts />
-      <RacialGapFacts />
+      <Switch>
+        <Route exact path={'/'} component={GenderGapFacts}/>
+        <Route exact path={'/agegapfacts'} component={AgeGapFacts}/>
+        <Route exact path={'/racialgapfacts'} component={RacialGapFacts}/>
+        <Redirect to='/'/>
+      </Switch>
     </main>
   )}
 }
 
-export default App;
+export default withRouter(App);
